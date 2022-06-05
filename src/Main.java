97e5d4dde6831f16c7d60f8fcb078ac6587e5233
Alexandr Trunov <trunovau@yandex.ru>
2022-06-05 21:16:42 +0300

    Output result to file

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,20 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) throws FileNotFoundException {
-
-        String separator = File.separator;
-        String path = "c:" + separator +"Users" + separator + "truno" + separator + "IdeaProjects" + separator + "CalculatorStudy" + separator + "inputFile.txt";
-        File inputFromFile = new File(path);
+        File inputFromFile = new File("inputFile");
+        File outputToFile = new File("OutputFile");
+        PrintWriter resultToFile = new PrintWriter(outputToFile);
 
         Scanner sc = new Scanner(inputFromFile);
 
         String[] nums = sc.nextLine().split(" ");
         String operator = nums[1];
+        double resultNumber;
 
         try {
 
@@ -21,32 +22,43 @@ public class Main {
             Double.parseDouble(nums[2]);
 
             if (nums[2].equals("0") && operator.equals("/")) {
-                System.out.println("Error! Division by zero");
+                resultToFile.println("Error! Division by zero");
+                resultToFile.close();
             }
             else {
 
                 switch (operator) {
                     case "+":
-                        System.out.println(Double.parseDouble(nums[0]) + Double.parseDouble(nums[2]));
+                        resultNumber = Double.parseDouble(nums[0]) + Double.parseDouble(nums[2]);
+                        resultToFile.println(resultNumber);
+                        resultToFile.close();
                         break;
                     case "-":
-                        System.out.println(Double.parseDouble(nums[0]) - Double.parseDouble(nums[2]));
+                        resultNumber = Double.parseDouble(nums[0]) - Double.parseDouble(nums[2]);
+                        resultToFile.println(resultNumber);
+                        resultToFile.close();
                         break;
                     case "*":
-                        System.out.println(Double.parseDouble(nums[0]) * Double.parseDouble(nums[2]));
+                        resultNumber = Double.parseDouble(nums[0]) * Double.parseDouble(nums[2]);
+                        resultToFile.println(resultNumber);
+                        resultToFile.close();
                         break;
                     case "/":
-                        System.out.println(Double.parseDouble(nums[0]) / Double.parseDouble(nums[2]));
+                        resultNumber = Double.parseDouble(nums[0]) / Double.parseDouble(nums[2]);
+                        resultToFile.println(resultNumber);
+                        resultToFile.close();
                         break;
                     default:
-                        System.out.println("Operation Error!");
+                        resultToFile.println("Operation Error!");
+                        resultToFile.close();
                         break;
 
                 }
             }
         }
         catch (Exception format) {
-            System.out.println("Error! Not number");
+            resultToFile.println("Error! Not number");
+            resultToFile.close();
         }
     }
 }
